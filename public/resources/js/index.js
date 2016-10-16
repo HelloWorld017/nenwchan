233afbc8b31a0fdf518f4b84ec5bde0dd675feba
HelloWorld017 <deu07115@gmail.com>
2016-10-16 16:37:21 +0900

    Updated layout, favicon, pjax

--- public/resources/js/index.js
+++ public/resources/js/index.js
@@ -1,13 +1,37 @@
-var main = $('main');
+(function(){
+	var main = $('main');
+	$(document).ready(function(){
+		$('.main-section').parallax({
+			imageSrc: './resources/image/parallax.jpg',
+			naturalWidth: 3840,
+			naturalHeight: 2400,
+			speed: 0.1
+		});
 
-$('.main-section').parallax({
-	imageSrc: './resources/image/parallax.jpg'
-});
+		$('.parallax-section').parallax({
+			imageSrc: './resources/image/parallax2.png',
+			naturalWidth: 4266,
+			naturalHeight: 2049,
+			speed: 0.1
+		});
 
-$('.section-scroll').click(function(e){
-	main.animate({
-		scrollTop: window.innerHeight
-	}, 300);
+		main.on('scroll', function(){
+			$(window).trigger('resize').trigger('scroll');
+		});
 
-	return false;
-});
+		$('a[data-href]').click(function(e){
+			main.animate({
+				scrollTop: main.scrollTop() + $($(this).attr('data-href')).position().top
+			}, 300);
+			return false;
+		});
+
+		$('.section-scroll').click(function(e){
+			main.animate({
+				scrollTop: window.innerHeight
+			}, 300);
+
+			return false;
+		});
+	});
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,39 @@
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
-	res.render('index');
+	res.render('frame', {
+		name: 'index'
+	});
+});
+
+router.get('/pjax', (req, res, next) => {
+	res.render('index', {}, (err, html) => {
+
+	});
+});
+
+router.get('/gallery', (req, res, next) => {
+	res.render('frame', {
+		name: 'gallery'
+	});
+});
+
+router.get('/gallery/pjax', (req, res, next) => {
+	res.render('gallery', {}, (err, html) => {
+
+	});
+});
+
+router.get('/service', (req, res, next) => {
+	res.render('frame', {
+		name: 'service'
+	});
+});
+
+router.get('/service/pjax', (req, res, next) => {
+	res.render('service', {}, (err, html) => {
+
+	});
 });
 
 module.exports = router;
